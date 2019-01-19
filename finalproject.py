91f535d66fd3ff47afcddd8b8cf23fb0adb3de40
Oded Leiba <oded@kzencorp.com>
2019-01-20 01:17:22 +0200

    aggregated proofs and batched validation with NI-PoE

--- finalproject.py
+++ finalproject.py
@@ -1,6 +1,7 @@
 import secrets
+from functools import reduce
 
-from helpfunctions import concat, generate_two_large_distinct_primes, hash_to_prime, mul_inv
+from helpfunctions import concat, generate_two_large_distinct_primes, hash_to_prime
 
 RSA_KEY_SIZE = 3072  # RSA key size for 128 bits of security (modulu size)
 RSA_PRIME_SIZE = int(RSA_KEY_SIZE / 2)
@@ -38,10 +39,29 @@ def prove_membership(A0, S, x, n):
         return A
 
 
-def prove_membership_with_PoE(g, S, x, n, w):
+def batch_prove_membership(A0, S, x_list, n):
+    A = A0
+    for element in S.keys():
+        if element not in x_list:
+            nonce = S[element]
+            A = pow(A, hash_to_prime(element, ACCUMULATED_PRIME_SIZE, nonce)[0], n)
+    return A
+
+
+def prove_membership_with_NIPoE(g, S, x, n, w):
     u = prove_membership(g, S, x, n)
     x_prime, x_nonce = hash_to_prime(x=x, nonce=S[x])
     (Q, l_nonce) = prove_exponentiation(u, x_prime, w, n)
+    return Q, l_nonce, u
+
+
+def batch_prove_membership_with_NIPoE(A0, S, x_list, n, w):
+    u = batch_prove_membership(A0, S, x_list, n)
+    primes_list = list(map(lambda x: hash_to_prime(x)[0], x_list))
+    # primes_list = list(map(lambda e: e[0], primes_nonces_list))
+    # nonces_list = list(map(lambda e: e[1], primes_nonces_list))
+    product = reduce(lambda first, second: first * second, primes_list, 1)
+    (Q, l_nonce) = prove_exponentiation(u, product, w, n)
     return Q, l_nonce, u
 
 
@@ -52,7 +72,7 @@ def prove_membership_with_PoE(g, S, x, n, w):
 #   w - the accumulator after the addition of x
 #   n - the modulu
 # Returns:
-#   Q, x - the PoE
+#   Q, x - the NIPoE
 #   nonce - the nonce used for hash_to_prime to receive l (for saving work to the verifier)
 def prove_exponentiation(u, x, w, n):
     l, nonce = hash_to_prime(concat(x, u, w))  # Fiat-Shamir instead of interactive challenge
@@ -65,6 +85,16 @@ def prove_exponentiation(u, x, w, n):
 # we pass the l_nonce just for speed up. The verifier has to reproduce l himself.
 def verify_exponentiation(Q, l_nonce, u, x, x_nonce, w, n):
     x = hash_to_prime(x=x, nonce=x_nonce)[0]
+    return _verify_exponentiation(Q, l_nonce, u, x, w, n)
+
+
+def batch_verify_membership_with_NIPoE(Q, l_nonce, u, x_list, x_nonces_list, w, n):
+    product = _calculate_primes_product(x_list, x_nonces_list)
+    return _verify_exponentiation(Q, l_nonce, u, product, w, n)
+
+
+# helper function, does not do hash_to_prime on x
+def _verify_exponentiation(Q, l_nonce, u, x, w, n):
     l = hash_to_prime(x=(concat(x, u, w)), nonce=l_nonce)[0]
     r = x % l
     # check (Q^l)(u^r) == w
@@ -84,4 +114,23 @@ def delete_element(A0, A, S, x, n):
 
 
 def verify(A, x, nonce, proof, n):
-    return pow(proof, hash_to_prime(x=x, num_of_bits=ACCUMULATED_PRIME_SIZE, nonce=nonce)[0], n) == A
+    return _verify(A, hash_to_prime(x=x, num_of_bits=ACCUMULATED_PRIME_SIZE, nonce=nonce)[0], proof, n)
+
+
+def batch_verify_membership(A, x_list, nonce_list, proof, n):
+    product = _calculate_primes_product(x_list, nonce_list)
+    return _verify(A, product, proof, n)
+
+
+def _calculate_primes_product(x_list, nonce_list):
+    agg_list = []
+    for i in range(len(x_list)):
+        agg_list.append([x_list[i], nonce_list[i]])
+    primes_list = map(lambda e: hash_to_prime(x=e[0], nonce=e[1])[0],  agg_list)
+    product = reduce(lambda first, second: first * second, primes_list, 1)
+    return product
+
+
+# helper function, does not do hash to prime.
+def _verify(A, x, proof, n):
+    return pow(proof, x, n) == A
--- test.py
+++ test.py
@@ -2,10 +2,17 @@ import secrets
 import math
 from helpfunctions import hash_to_prime, is_prime, generate_large_prime
 from finalproject import setup, add_element, prove_membership, delete_element, verify, \
-        prove_membership_with_PoE, verify_exponentiation
+        prove_membership_with_NIPoE, verify_exponentiation, batch_prove_membership, batch_verify_membership, \
+        batch_prove_membership_with_NIPoE, batch_verify_membership_with_NIPoE
 from unittest import TestCase
 import time
 
+def create_list(size):
+        res = []
+        for i in range(size):
+                x = secrets.randbelow(pow(2, 256))
+                res.append(x)
+        return res
 
 class AccumulatorTest(TestCase):
         def test_hash_to_prime(self):
@@ -54,6 +61,38 @@ class AccumulatorTest(TestCase):
                 A1 = add_element(A0, S, x0, n)
                 A2 = add_element(A1, S, x1, n)
 
-                Q, l_nonce, u = prove_membership_with_PoE(A0, S, x0, n, A2)
+                Q, l_nonce, u = prove_membership_with_NIPoE(A0, S, x0, n, A2)
                 is_valid = verify_exponentiation(Q, l_nonce, u, x0, S[x0], A2, n)
+                self.assertTrue(is_valid)
+
+        def test_batch_proof_of_membership(self):
+                n, A0, S = setup()
+
+                elements_list = create_list(10)
+
+                A = A0
+                for x in elements_list:
+                        A = add_element(A, S, x, n)
+                A_final = A
+
+                elements_to_prove_list = [elements_list[4], elements_list[7], elements_list[8]]
+                A_intermediate = batch_prove_membership(A0, S, elements_to_prove_list, n=n)
+                nonces_list = list(map(lambda e: hash_to_prime(e)[1], elements_to_prove_list))
+                is_valid = batch_verify_membership(A_final, elements_to_prove_list, nonces_list, A_intermediate, n)
+                self.assertTrue(is_valid)
+
+        def test_batch_proof_of_membership_with_NIPoE(self):
+                n, A0, S = setup()
+
+                elements_list = create_list(10)
+
+                A = A0
+                for x in elements_list:
+                        A = add_element(A, S, x, n)
+                A_final = A
+
+                elements_to_prove_list = [elements_list[4], elements_list[7], elements_list[8]]
+                Q, l_nonce, u = batch_prove_membership_with_NIPoE(A0, S, elements_to_prove_list, n, A_final)
+                nonces_list = list(map(lambda e: hash_to_prime(e)[1], elements_to_prove_list))
+                is_valid = batch_verify_membership_with_NIPoE(Q, l_nonce, u, elements_to_prove_list, nonces_list, A_final, n)
                 self.assertTrue(is_valid)
